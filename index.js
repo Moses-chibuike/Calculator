21f07bba7593f1cfc9c930886aee4e9522413f6c
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2022-08-13 01:31:56 +0100

    final commitment

--- index.js
+++ index.js
@@ -13,6 +13,14 @@
                     display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
+            case '=':
+
+                 try{
+                    display.innerText = eval(display.innerText); 
+                 } catch {
+                    display.innerText = 'Error!';
+                 }
+                break;
             default:
                     display.innerText += e.target.innerText;
         }
